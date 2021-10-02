25823935ff766f8ed7ce60a3f6664a008a50adde
sad1go0 <sayidullohabbasov0909@gmail.com>
2021-10-03 00:56:38 +0500

    added bot join validation

--- validation/joined.js
+++ validation/joined.js
@@ -5,6 +5,7 @@ bot.on(':new_chat_members', async ctx => {
   await ctx.deleteMessage();
   ctx.message.new_chat_members.forEach(u => {
     if(u){
+      if(u.is_bot) return ctx.banChatMember(u.id);
       const first_name = u.first_name;
       const last_name = u.last_name || '';
       const id = u.id;
